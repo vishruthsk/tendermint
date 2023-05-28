d33aedee4d81175477c883d11f71b0dec9d6fccc
vishruthsk <vishruthsk@gmail.com>
2023-05-28 16:41:52 +0530

    commit

--- types/block.go
+++ types/block.go
@@ -15,7 +15,7 @@ import (
 	"github.com/tendermint/tendermint/libs/bits"
 	tmbytes "github.com/tendermint/tendermint/libs/bytes"
 	tmmath "github.com/tendermint/tendermint/libs/math"
-	tmproto "github.com/tendermint/tendermint/proto/types/keys"
+	tmproto "github.com/tendermint/tendermint/proto/types"
 	tmversion "github.com/tendermint/tendermint/proto/version"
 	"github.com/tendermint/tendermint/version"
 )
